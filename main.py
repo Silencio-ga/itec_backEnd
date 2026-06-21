23029ef45068e55880399a4da04ca7fde8151df5
silencio <silencioxxx10@gmail>
2026-06-21 18:38:54 -0300

    practico_2: pydantic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from fastapi import FastAPI, Body, Path, Query
+from fastapi import FastAPI, Body, Path, Query, HTTPException
 from typing import Annotated
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 app.title = "Nuestra primera APP"
@@ -11,15 +11,27 @@ app.title = "Nuestra primera APP"
 #==========
 
 class serie(BaseModel):
-    id: int
-    nombre: str 
-    genero: str
-    score: int 
+    id: Annotated[int, Field(gt=0)]
+    nombre: Annotated[
+        str,
+        Field(min_length=3, max_length=35, examples= ["Breaking Bad"], default= "Stranger Things"),
+        ]
+    genero: Annotated[
+        str,
+        Field(min_length=3, max_length=35, examples= ["Drama"], default= "Ciencia ficción")
+    ]
+    score: Annotated[int, Field(gt=0, le=100, examples=[50], default=50)]
 
 class serieUpdate(BaseModel):
-    nombre: str 
-    genero: str
-    score: int 
+    nombre: Annotated[
+        str,
+        Field(min_length=3, max_length=35, examples= ["Breaking Bad"], default= "Stranger Things"),
+        ]
+    genero: Annotated[
+        str,
+        Field(min_length=3, max_length=35, examples= ["Drama"], default= "Ciencia ficción")
+    ]
+    score: Annotated[int, Field(gt=0, le=100, examples=[50], default=50)]
 
 #========
 # Datos
@@ -39,23 +51,34 @@ series = [
 #============
 
 # lista de series
-@app.get("/series", tags=["buscar series"])
+@app.get("/series", tags=["buscar series"], response_model=list[serie])
 def obtener_series() -> list[serie]:
     return series
 
 # buscar serie por ID
-@app.get("/series{id}", tags=["buscar series"])
+@app.get("/series/{id}", 
+    tags=["buscar series"], 
+    response_model=serie,
+    responses={
+        404: {
+        "description": "Serie no econtrado", 
+        "content": {
+            "application/json": {"example": {"detail": "Serie no econtrado"}}
+            }
+        }
+    },
+)
 def obtener_serie(id: Annotated[int, Path(gt=0)]) -> serie:
     for serie in series:
         if serie["id"] == id:
             return serie
-    return []
+    raise HTTPException(status_code=404, detail="serie no encontrado")
 
 #============
 #    POST 
 #============
 
-@app.post("/agregar-serie", tags=["añadir serie"])
+@app.post("/agregar-serie", tags=["añadir serie"], response_model=list[serie])
 def agregar_serie(serie:serie) -> list[serie]:
     series.append(serie.model_dump())
     return series
@@ -64,27 +87,49 @@ def agregar_serie(serie:serie) -> list[serie]:
 #    PUT 
 #============
 
-@app.put("/editar-juego/{id}", tags=["editar"])
+@app.put(
+    "/editar-serie/{id}", 
+    tags=["editar"],
+    response_model=serieUpdate,
+    responses={
+        404: {
+        "description": "Serie no econtrado", 
+        "content": {
+            "application/json": {"example": {"detail": "Serie no econtrado"}}
+            }
+        }
+    },)
 def editar_serie(
     id: int,
-    nombre: serieUpdate) -> serieUpdate: 
+    serie: serieUpdate) -> serieUpdate: 
     for s in series:
         if s["id"] == id:
             s["nombre"] = serie.nombre
             s["genero"] = serie.genero
             s["score"] = serie.score
             return serie
-    return {"mensaje": "serie no encontrado"}
+    raise HTTPException(status_code=404, detail="serie no encontrado")
 
 #============
 #   DELETE 
 #============
 
-@app.delete("/borrar-serie/{id}", tags=["elimina"])
+@app.delete("/borrar-serie/{id}", 
+    tags=["elimina"], 
+    response_model=list[serie],
+    responses={
+        404: {
+        "description": "Serie no econtrado", 
+        "content": {
+            "application/json": {"example": {"detail": "Serie no econtrado"}}
+            }
+        }
+    },
+)
 def borrar_serie(id: int) -> list[serie]:
     for serie in series:
         if serie["id"] == id:
             series.remove(serie)
             return series
-    return {"mensaje": "serie no encontrado"}
+    raise HTTPException(status_code=404, detail="serie no encontrado")
 
