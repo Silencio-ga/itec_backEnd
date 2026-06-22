abc320f6c3a1cf193edcd49d429b534605797daa
silencio <silencioxxx10@gmail>
2026-06-22 17:36:21 -0300

    clase CORS

--- src/main.py
+++ src/main.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from src.routers.series_router import series_router
 
 app = FastAPI()
-app.title = "Nuestra primera APP"
+app.title = "Gabriel acuña - tp evaluativo FastAPI"
+
+origenes_permitidos = [
+    "http://localhost:3000",
+    "http://127.0.0.1:5500",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origenes_permitidos,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router=series_router, prefix="/series")
-
-
--- src/routers/series_router.py
+++ src/routers/series_router.py
@@ -1,79 +1,86 @@
 from typing import Annotated
-from fastapi import HTTPException, Path, APIRouter
+
+from fastapi import APIRouter, HTTPException, Path
 
 from src.schemas.series_schemas import serie, serieUpdate
 
 series_router = APIRouter()
 
-#========
+# ========
 # Datos
-#========
+# ========
 
 series: list[serie] = [
-    {"id": 1,"nombre": "Breaking Bad","genero": "Drama", "score": 54},
-    {"id": 2,"nombre": "Stranger Things","genero": "Ciencia ficción", "score": 75},
-    {"id": 3,"nombre": "Game of Thrones","genero": "Fantasía", "score": 12},
-    {"id": 4,"nombre": "The Office","genero": "Comedia", "score": 56},
-    {"id": 5,"nombre": "Dark","genero": "Misterio", "score": 93},
-    {"id": 6,"nombre": "The Mandalorian","genero": "Acción", "score": 67}
+    {"id": 1, "nombre": "Breaking Bad", "genero": "Drama", "score": 54},
+    {"id": 2, "nombre": "Stranger Things", "genero": "Ciencia ficción", "score": 75},
+    {"id": 3, "nombre": "Game of Thrones", "genero": "Fantasía", "score": 12},
+    {"id": 4, "nombre": "The Office", "genero": "Comedia", "score": 56},
+    {"id": 5, "nombre": "Dark", "genero": "Misterio", "score": 93},
+    {"id": 6, "nombre": "The Mandalorian", "genero": "Acción", "score": 67},
 ]
 
-#============
-#    GET 
-#============
+# ============
+#    GET
+# ============
+
 
 # lista de series
 @series_router.get("/", tags=["buscar series"], response_model=list[serie])
 def obtener_series() -> list[serie]:
     return series
 
+
 # buscar serie por ID
-@series_router.get("/{id}", 
-    tags=["buscar series"], 
+@series_router.get(
+    "/{id}",
+    tags=["buscar series"],
     response_model=serie,
     responses={
         404: {
-        "description": "Serie no econtrado", 
-        "content": {
-            "application/json": {"example": {"detail": "Serie no econtrado"}}
-            }
+            "description": "Serie no econtrado",
+            "content": {
+                "application/json": {"example": {"detail": "Serie no econtrado"}}
+            },
         }
     },
 )
 def obtener_serie(id: Annotated[int, Path(gt=0)]) -> serie:
-    for serie in series:
-        if serie["id"] == id:
-            return serie
+    for s in series:
+        if s["id"] == id:
+            return s
     raise HTTPException(status_code=404, detail="serie no encontrado")
 
-#============
-#    POST 
-#============
+
+# ============
+#    POST
+# ============
+
 
 @series_router.post("/", tags=["añadir serie"], response_model=list[serie])
-def agregar_serie(serie:serie) -> list[serie]:
+def agregar_serie(serie: serie) -> list[serie]:
     series.append(serie.model_dump())
     return series
 
-#============
-#    PUT 
-#============
+
+# ============
+#    PUT
+# ============
+
 
 @series_router.put(
-    "/{id}", 
+    "/{id}",
     tags=["editar"],
     response_model=serieUpdate,
     responses={
         404: {
-        "description": "Serie no econtrado", 
-        "content": {
-            "application/json": {"example": {"detail": "Serie no econtrado"}}
-            }
+            "description": "Serie no econtrado",
+            "content": {
+                "application/json": {"example": {"detail": "Serie no econtrado"}}
+            },
         }
-    },)
-def editar_serie(
-    id: int,
-    serie: serieUpdate) -> serieUpdate: 
+    },
+)
+def editar_serie(id: Annotated[int, Path(gt=0)], serie: serieUpdate) -> serieUpdate:
     for s in series:
         if s["id"] == id:
             s["nombre"] = serie.nombre
@@ -82,26 +89,28 @@ def editar_serie(
             return serie
     raise HTTPException(status_code=404, detail="serie no encontrado")
 
-#============
-#   DELETE 
-#============
 
-@series_router.delete("/{id}", 
-    tags=["elimina"], 
+# ============
+#   DELETE
+# ============
+
+
+@series_router.delete(
+    "/{id}",
+    tags=["elimina"],
     response_model=list[serie],
     responses={
         404: {
-        "description": "Serie no econtrado", 
-        "content": {
-            "application/json": {"example": {"detail": "Serie no econtrado"}}
-            }
+            "description": "Serie no econtrado",
+            "content": {
+                "application/json": {"example": {"detail": "Serie no econtrado"}}
+            },
         }
     },
 )
-def borrar_serie(id: int) -> list[serie]:
-    for serie in series:
-        if serie["id"] == id:
-            series.remove(serie)
+def borrar_serie(id: Annotated[int, Path(gt=0)]) -> list[serie]:
+    for s in series:
+        if s["id"] == id:
+            series.remove(s)
             return series
     raise HTTPException(status_code=404, detail="serie no encontrado")
-
